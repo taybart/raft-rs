02ad7ce3cc91fcb0837497c49d550160b757f848
Taylor Bartlett <taybart@gmail.com>
2022-11-06 20:37:14 -0700

    feat: move to protobuf

--- build.rs
+++ build.rs
@@ -1,7 +1,12 @@
-extern crate capnpc;
+use std::io::Result;
+// extern crate capnpc;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Ok(capnpc::CompilerCommand::new()
-        .file("proto/raft.capnp")
-        .run()?)
+// fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<()> {
+    // Ok(capnpc::CompilerCommand::new()
+    //     .file("proto/raft.capnp")
+    //     .run()?)
+
+    prost_build::compile_protos(&["src/proto/raft.proto"], &["src/"])?;
+    Ok(())
 }
