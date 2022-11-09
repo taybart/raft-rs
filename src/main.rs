414dcbaf79367a82e4e554be2b2b99bd0df725db
Taylor Bartlett <taybart@gmail.com>
2022-11-08 17:39:39 -0700

    working tcp call

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 mod raft;
 use raft::{protocol, rpc};
 
-// const ADDRESS: &'static str = "127.0.0.1:9090";
+const ADDRESS: &'static str = "127.0.0.1:9090";
+
+const SERVER: bool = false;
 
 #[tokio::main]
 async fn main() {
-    rpc::request_vote(protocol::VoteRequest {
-        term: 1,
-        candidate_id: 0,
-        last_log_index: 0,
-        last_log_term: 0,
-    })
-    .await
-    .unwrap();
+    if !SERVER {
+        let c = rpc::Client {
+            addr: ADDRESS.to_string(),
+        };
+        c.request_vote(protocol::VoteRequest {
+            other_thing: "woot".to_string(),
+            term: 1,
+            candidate_id: 0,
+            last_log_index: 0,
+            last_log_term: 0,
+        })
+        .await
+        .unwrap();
+    } else {
+        let s = rpc::Server {
+            addr: ADDRESS.to_string(),
+        };
+        let _ = s.listen().await.map_err(|_| "woah");
+    }
 }
--- src/raft/rpc.rs
+++ src/raft/rpc.rs
@@ -1,72 +1,143 @@
 use super::protocol;
-use prost::Message;
+use prost::{bytes::Bytes, Message};
+use std::io::{Read, Write};
+use std::net::TcpStream;
 
-pub async fn request_vote(req: protocol::VoteRequest) -> Result<protocol::VoteResponse, String> {
-    let mut res = protocol::VoteResponse::default();
-    res.term = req.term;
-    let mut buf = Vec::new();
-    res.encode(&mut buf).map_err(|_| "issue")?;
-    println!("test? {}", res.term);
-    Ok(res)
+pub struct Client {
+    pub addr: String,
 }
 
-// pub async fn request_vote(req: protocol::VoteRequest) -> Result<protocol::VoteResponse, String> {
-//     let mut res = protocol::VoteResponse::default();
-//     res.term = req.term;
-//     let mut buf = Vec::new();
-//     res.encode(&mut buf).map_err(|_| "issue")?;
-//     // Ok(buf)
-//     // let res = raft::VoteResponse::decode(req.as_slice()).unwrap();
+impl Client {
+    pub async fn request_vote(
+        self,
+        req: protocol::VoteRequest,
+    ) -> Result<protocol::VoteResponse, String> {
+        let _b_res = self.call(req.encode_to_vec()).unwrap();
+        Ok(protocol::VoteResponse::default())
+    }
 
-//     println!("test? {}", res.term);
-//     Ok(res)
-// }
+    fn call(self, req: Vec<u8>) -> Result<Vec<u8>, String> {
+        let mut client = TcpStream::connect(self.addr).map_err(|_| "failed to connect")?;
 
-// pub async fn connect(host: &String, port: &u16) -> Result<(), String> {
-//     let addr = format!("{}:{}", host, port);
-//     let client = tokio::net::TcpStream::connect(addr)
-//         .await
-//         .map_err(|_| "failed to connect")?;
+        println!("connected...");
 
-//     let (mut reader, mut writer) = client.into_split();
+        client
+            .write_all(&req)
+            .map_err(|_| "failed to write request")?;
 
-//     let client_read =
-//         tokio::spawn(async move { tokio::io::copy(&mut reader, &mut tokio::io::stdout()).await });
+        println!("wrote {} bytes", req.len());
 
-//     let client_write =
-//         tokio::spawn(async move { tokio::io::copy(&mut tokio::io::stdin(), &mut writer).await });
+        let buf = Vec::new();
 
-//     tokio::select! {
-//         _ = client_read => {}
-//         _ = client_write => {}
-//     }
+        // client
+        //     .read_to_end(&mut buf)
+        //     .map_err(|_| "failed to write request")?;
 
-//     Ok(())
-// }
+        Ok(buf)
+    }
 
-// pub async fn listen(host: &String, port: &u16) -> Result<(), String> {
-//     let addr = format!("{}:{}", host, port);
-//     let listener = tokio::net::TcpListener::bind(addr)
-//         .await
-//         .map_err(|_| "failed to bind address")?;
+    // async fn call(self) -> Result<Vec<u8>, String> {
+    //     // let addr = format!("{}:{}", host, port);
+    //     let client = tokio::net::TcpStream::connect(self.addr)
+    //         .await
+    //         .map_err(|_| "failed to connect")?;
 
-//     let (handle, _) = listener
-//         .accept()
-//         .await
-//         .map_err(|_| "failed to accept incoming connection")?;
+    //     let (mut reader, mut writer) = client.into_split();
 
-//     let (mut reader, mut writer) = handle.into_split();
+    //     let client_read =
+    //         tokio::spawn(
+    //             async move { tokio::io::copy(&mut reader, &mut tokio::io::stdout()).await },
+    //         );
 
-//     let client_read =
-//         tokio::spawn(async move { tokio::io::copy(&mut reader, &mut tokio::io::stdout()).await });
+    //     let client_write =
+    //         tokio::spawn(
+    //             async move { tokio::io::copy(&mut tokio::io::stdin(), &mut writer).await },
+    //         );
 
-//     let client_write =
-//         tokio::spawn(async move { tokio::io::copy(&mut tokio::io::stdin(), &mut writer).await });
+    //     tokio::select! {
+    //         _ = client_read => {}
+    //         _ = client_write => {}
+    //     }
 
-//     tokio::select! {
-//         _ = client_read => {}
-//         _ = client_write => {}
-//     }
+    //     Ok(())
+    // }
+}
 
-//     Ok(())
-// }
+pub struct Server {
+    pub addr: String,
+}
+
+impl Server {
+    // pub async fn request_vote(
+    //     self,
+    //     req: protocol::VoteRequest,
+    // ) -> Result<protocol::VoteResponse, String> {
+    //     Ok(())
+    // }
+    pub async fn listen(self) -> Result<(), String> {
+        // let addr = format!("{}:{}", host, port);
+        let listener =
+            std::net::TcpListener::bind(self.addr).map_err(|_| "failed to bind address")?;
+
+        println!("listening...");
+
+        for stream in listener.incoming() {
+            match stream {
+                Ok(mut s) => {
+                    println!("Connection accepted");
+
+                    let mut buf = [0; 128];
+                    let mut read_bytes = 0;
+                    while read_bytes == 0 {
+                        read_bytes = s.read(&mut buf).map_err(|_| "failed to read from socket")?;
+                        if read_bytes > 0 {
+                            println!("received bytes {}", read_bytes);
+                        }
+                    }
+
+                    let req =
+                        protocol::VoteRequest::decode(Bytes::copy_from_slice(&buf[0..read_bytes]))
+                            .unwrap();
+                    println!("test? {}", req.other_thing);
+                }
+                Err(e) => {
+                    println!("Error while accepting incoming connection - {}", e);
+                }
+            }
+        }
+
+        Ok(())
+    }
+    // pub async fn listen(self) -> Result<(), String> {
+    //     // let addr = format!("{}:{}", host, port);
+    //     let listener = tokio::net::TcpListener::bind(self.addr)
+    //         .await
+    //         .map_err(|_| "failed to bind address")?;
+
+    //     println!("listening...");
+
+    //     let (handle, _) = listener
+    //         .accept()
+    //         .await
+    //         .map_err(|_| "failed to accept incoming connection")?;
+
+    //     let (mut reader, mut writer) = handle.into_split();
+
+    //     let client_read =
+    //         tokio::spawn(
+    //             async move { tokio::io::copy(&mut reader, &mut tokio::io::stdout()).await },
+    //         );
+
+    //     let client_write =
+    //         tokio::spawn(
+    //             async move { tokio::io::copy(&mut tokio::io::stdin(), &mut writer).await },
+    //         );
+
+    //     tokio::select! {
+    //         _ = client_read => {}
+    //         _ = client_write => {}
+    //     }
+
+    //     Ok(())
+    // }
+}
