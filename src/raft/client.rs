a2b51ae70d3f152005373183dc4863535fa64ef5
Taylor Bartlett <taybart@gmail.com>
2022-11-10 17:33:18 -0700

    feat: bigger bufs and flatter imports

--- src/raft/client.rs
+++ src/raft/client.rs
@@ -1,4 +1,4 @@
-use super::protocol;
+use super::protocol::{AppendRequest, AppendResult, Function, Rpc, VoteRequest, VoteResponse};
 use prost::{bytes::Bytes, Message};
 use std::io::{Read, Write};
 use std::net::TcpStream;
@@ -8,41 +8,33 @@ pub struct Client {
 }
 
 impl Client {
-    pub async fn request_vote(
-        &self,
-        req: protocol::VoteRequest,
-    ) -> Result<protocol::VoteResponse, String> {
-        let mut rpc = protocol::Rpc::default();
-        rpc.set_call(protocol::rpc::Func::RequestVote);
+    pub async fn request_vote(&self, req: VoteRequest) -> Result<VoteResponse, String> {
+        let mut rpc = Rpc::default();
+        rpc.set_call(Function::RequestVote);
         rpc.request = req.encode_to_vec();
-        let b_res = self.call(rpc).unwrap();
-        let res = protocol::VoteResponse::decode(Bytes::copy_from_slice(&b_res))
+        let b_res = self.call(rpc)?;
+        let res = VoteResponse::decode(Bytes::copy_from_slice(&b_res))
             .map_err(|_| "failed to decode response")?;
         Ok(res)
     }
-    pub async fn append_entries(
-        &self,
-        req: protocol::AppendRequest,
-    ) -> Result<protocol::AppendResult, String> {
-        let mut rpc = protocol::Rpc::default();
-        rpc.set_call(protocol::rpc::Func::AppendEntries);
+    pub async fn append_entries(&self, req: AppendRequest) -> Result<AppendResult, String> {
+        let mut rpc = Rpc::default();
+        rpc.set_call(Function::AppendEntries);
         rpc.request = req.encode_to_vec();
-        let b_res = self.call(rpc).unwrap();
-        let res = protocol::AppendResult::decode(Bytes::copy_from_slice(&b_res))
+        let b_res = self.call(rpc)?;
+        let res = AppendResult::decode(Bytes::copy_from_slice(&b_res))
             .map_err(|_| "failed to decode response")?;
         Ok(res)
     }
 
-    fn call(&self, req: protocol::Rpc) -> Result<Vec<u8>, String> {
+    fn call(&self, req: Rpc) -> Result<Vec<u8>, String> {
         let mut client = TcpStream::connect(self.addr.clone()).map_err(|_| "failed to connect")?;
-
-        println!("connected...");
 
         client
             .write_all(&req.encode_to_vec())
             .map_err(|_| "failed to write request")?;
 
-        let mut buf = vec![0; 1024];
+        let mut buf = vec![0; 4096];
         let n = client
             .read(&mut buf)
             .map_err(|_| "failed to read response")?;
--- src/raft/server.rs
+++ src/raft/server.rs
@@ -1,85 +1,250 @@
-use super::protocol;
+use super::protocol::{AppendRequest, AppendResult, LogEntry, VoteRequest, VoteResponse};
+use super::protocol::{Function, Rpc};
 use prost::{bytes::Bytes, Message};
-use tokio::net::{TcpListener, TcpStream};
+use std::{
+    sync::{Arc, Mutex},
+    time::Instant,
+};
+use tokio::{
+    net::{TcpListener, TcpStream},
+    time::{sleep, Duration},
+};
 
+const ELECTION_TIMEOUT_MS: u128 = 3_000;
+
+#[derive(Default)]
 pub struct Server {
-    // id: u64,
     pub addr: String,
 }
 
+#[derive(Default, PartialEq)]
+enum Role {
+    #[default]
+    Follower,
+    Candidate,
+    Leader,
+}
+
+struct WrapInstant(std::time::Instant);
+
+impl Default for WrapInstant {
+    fn default() -> Self {
+        WrapInstant(Instant::now())
+    }
+}
+
+type SState = Arc<Mutex<State>>;
+#[derive(Default)]
+pub struct State {
+    /* internal */
+    id: u64,
+    role: Role,
+    current_term: u64, // monotonic
+    _voted_for: String,
+    log: Vec<LogEntry>,
+
+    last_leader_contact: WrapInstant,
+
+    /* Voliatile */
+    // all servers
+    commit_index: u64,
+    last_applied: u64,
+    // leaders (reinitialized after election)
+    _next_index: Vec<u64>,
+    _match_index: Vec<u64>,
+
+    friends: Vec<String>,
+}
+
+impl State {
+    pub fn new(friends: Vec<String>) -> State {
+        let mut s: State = Default::default();
+        s.friends = friends;
+        s
+    }
+}
+
 impl Server {
-    pub fn handle_append_entries(req: protocol::AppendRequest) -> Vec<u8> {
+    pub fn handle_append_entries(state: SState, mut req: AppendRequest) -> Vec<u8> {
+        let mut state = state.lock().unwrap();
+
+        let mut ret = AppendResult {
+            term: state.current_term,
+            success: true,
+        };
+
         // heartbeat
         if req.entries.len() == 0 {
             println!("heartbeat");
-            return protocol::AppendResult {
-                term: req.term,
-                success: true,
-            }
-            .encode_to_vec();
+            return ret.encode_to_vec();
         }
-        println!("Vote request in term {}!", req.term);
-        Vec::new()
+
+        // they are behind
+        if req.term < state.current_term {
+            ret.success = false;
+            return ret.encode_to_vec();
+        }
+
+        // if req.leader_id != state.
+
+        // we are behind
+        if req.term > state.current_term {
+            state.role = Role::Follower;
+            state.current_term = req.term;
+        }
+
+        // jumping the line?
+        // let entry = state.log.get(state.current_term);
+
+        state.log.append(&mut req.entries);
+        state.commit_index += 1;
+
+        println!(
+            "updated log idx({}) {:?} from {}",
+            state.commit_index,
+            state.log.len(),
+            req.id,
+        );
+
+        state.last_leader_contact = WrapInstant(Instant::now());
+        ret.encode_to_vec()
     }
-    pub fn handle_vote_request(req: protocol::VoteRequest) -> Vec<u8> {
+    pub fn handle_vote_request(state: SState, req: VoteRequest) -> Vec<u8> {
         println!("Vote request in term {}!", req.term);
 
-        return protocol::VoteResponse {
+        let mut ret = VoteResponse {
             term: req.term,
             vote_grated: true,
+        };
+        let mut state = state.lock().unwrap();
+
+        if req.term < state.current_term {
+            ret.vote_grated = false;
+            return ret.encode_to_vec();
         }
-        .encode_to_vec();
+
+        // we are behind
+        if req.term > state.current_term {
+            state.role = Role::Follower;
+            state.current_term = req.term;
+        }
+
+        /*
+         * If votedFor is null or candidateId, and candidate’s log is at
+         * least as up-to-date as receiver’s log, grant vote
+         */
+        if req.candidate_id == state.id {
+            println!("someone voted for me!");
+            state.role = Role::Leader;
+        }
+
+        ret.encode_to_vec()
     }
 
-    async fn process(stream: TcpStream) -> Result<(), String> {
-        println!("Connection accepted");
+    async fn process(state: SState, stream: TcpStream) -> Result<(), String> {
+        let now = Instant::now();
 
-        let mut buf = vec![0; 1024];
+        stream
+            .readable()
+            .await
+            .map_err(|_| "could not check if stream is readable")?;
+
+        let mut buf = vec![0; 4096];
         let _n = match stream.try_read(&mut buf) {
             Ok(n) => buf.truncate(n),
             Err(e) => {
                 eprintln!("failed to read from socket; err = {:?}", e)
             }
         };
-        let rpc_req = protocol::Rpc::decode(Bytes::copy_from_slice(&buf)).unwrap();
-        match protocol::rpc::Func::from_i32(rpc_req.call) {
-            Some(protocol::rpc::Func::RequestVote) => {
-                let req = protocol::VoteRequest::decode(Bytes::copy_from_slice(&rpc_req.request))
-                    .unwrap();
-                let res = Self::handle_vote_request(req);
-                let n = stream
-                    .try_write(&res)
-                    .map_err(|e| format!("write res failed {}", e))?;
-                println!("wrote {} bytes", n)
+        let rpc_req = Rpc::decode(Bytes::copy_from_slice(&buf)).unwrap();
+        let res = match Function::from_i32(rpc_req.call) {
+            Some(Function::RequestVote) => {
+                let req = VoteRequest::decode(Bytes::copy_from_slice(&rpc_req.request)).unwrap();
+                Self::handle_vote_request(state, req)
             }
-            Some(protocol::rpc::Func::AppendEntries) => {
-                let req = protocol::AppendRequest::decode(Bytes::copy_from_slice(&rpc_req.request))
-                    .unwrap();
-                let res = Self::handle_append_entries(req);
-                let n = stream
-                    .try_write(&res)
-                    .map_err(|e| format!("write res failed {}", e))?;
-                println!("wrote {} bytes", n)
+            Some(Function::AppendEntries) => {
+                let req = AppendRequest::decode(Bytes::copy_from_slice(&rpc_req.request)).unwrap();
+                Self::handle_append_entries(state, req)
             }
             None => {
-                eprintln!("unknown rpc")
+                panic!("unknown rpc")
             }
-        }
+        };
+
+        // we already checked (+panicked) if call is known above
+        let func = Function::from_i32(rpc_req.call)
+            .expect("unknown rpc")
+            .as_str_name();
+        stream.try_write(&res).expect("writing response failed");
+        println!("{} done in {}μs", func, now.elapsed().as_micros());
         Ok(())
     }
-    pub async fn listen(self) -> Result<(), String> {
-        let listener = TcpListener::bind(self.addr)
-            .await
-            .map_err(|_| "failed to bind address")?;
+    async fn follower(state: SState) {
+        loop {
+            // 20ms tick
+            sleep(Duration::from_millis(20)).await;
+            let mut s = state.lock().unwrap();
+            if s.last_leader_contact.0.elapsed().as_millis() > ELECTION_TIMEOUT_MS
+                && s.role != Role::Leader
+            {
+                s.role = Role::Candidate;
+                s.current_term += 1;
+                println!(
+                    "no contact from leader, requesting election for term {}",
+                    s.current_term
+                );
 
-        println!("listening...");
+                let req = VoteRequest {
+                    term: s.current_term,
+                    candidate_id: s.id,
+                    last_log_index: s.last_applied,
+                    last_log_term: s.current_term - 1,
+                };
+                println!("lets elect a new leader...why not me?");
+                for friend in &s.friends {
+                    println!("hello friend:{:?} my name is {}\n{:?}", friend, s.id, req);
+                }
+                // assuming that went well
+                s.last_leader_contact.0 = Instant::now();
+                // elected!
+                // return;
+            }
+        }
+    }
+
+    pub async fn listen(self) -> Result<(), String> {
+        let listener = TcpListener::bind(self.addr.clone())
+            .await
+            .expect("failed to bind address");
+        println!("listening on {}...", self.addr);
+
+        // init state
+        let state = Arc::new(Mutex::new(State::new(vec![
+            "http://127.0.0.1:9091".to_string(),
+            "http://127.0.0.1:9092".to_string(),
+            "http://127.0.0.1:9093".to_string(),
+        ])));
+
+        {
+            // internal timer
+            let state = state.clone();
+            tokio::spawn(async move {
+                // loop to change state?
+                Self::follower(state).await
+            });
+        }
         loop {
             let (socket, _) = listener
                 .accept()
                 .await
                 .map_err(|_| "failed to accept incoming connection")?;
+
+            let state = state.clone();
             tokio::spawn(async move {
-                let _ = Self::process(socket).await;
+                match Self::process(state, socket).await {
+                    Ok(_) => {}
+                    Err(e) => eprintln!("{}", e),
+                };
             });
         }
     }
